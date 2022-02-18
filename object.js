cae9612b152c9f3924b216f51a448e4cc84c0e82
quarentyne <peresada1997@gmail.com>
2022-02-18 19:02:22 +0200

    nabroski

--- object.js
+++ object.js
@@ -1,2 +1,12 @@
 'use strict';
 
+class Client{
+  constructor(surname, name, patronymic, isActive) {
+    this.surname = surname;
+    this.name = name;
+    this.patronymic = patronymic;
+    this.isActive = isActive;
+  }
+}
+let Igor = new Client('Ivanov', 'Ivan', 'Petrovich', true)
+console.log(Igor);
