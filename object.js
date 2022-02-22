73616dbad9c48f6a46d274dc57336eb8ac636fef
quarentyne <peresada1997@gmail.com>
2022-02-22 19:24:11 +0200

    Hm, i can do it, but why so many cicles?

--- object.js
+++ object.js
@@ -1,14 +1,5 @@
 'use strict';
 
-async function api() {
-  let promise = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
-  let result = await promise.json();
-  return result;
-}
-
-let result = api();
-result.then(value => { console.log(value) }, value => { throw value });
-
 const bank = [];
 
 class Client{
@@ -18,7 +9,7 @@ class Client{
     this.joinDate = new Date();
     this.creditAccounts = [];
     this.debetAccounts = [];
-    this.id = bank.length;
+    this.id = Client.count++;
   }
 
   setDebetAccount(currency, balance, expirationDate, isActive, lastActiveDate) {
@@ -48,11 +39,95 @@ class Client{
     });
   }
 }
+Client.count = 0;
+
 let Petr = new Client('Petr Nikiforovich Muhin', true);
 bank.push(Petr);
 let Ivan = new Client('Ivan Petrovich Chushkin', true);
-bank.push(Ivan)
+bank.push(Ivan);
 Petr.setCreditAccount('USD', 10000, 5000, '20.01.2023', true, '21.02.2022');
-Petr.setDebetAccount('UAH', 1200, '11.12.2024', true, '21.02.2022');
+Petr.setDebetAccount('USD', 1200, '11.12.2024', true, '21.02.2022');
 Ivan.setDebetAccount('EUR', 100, '09.09.2029', false, '02.08.2009');
+Ivan.setDebetAccount('RUR', 10100, '09.09.2029', false, '02.08.2009');
+Ivan.setCreditAccount('UAH', 12000, 200000, '10.02.2022', false, '10.02.2022');
+let Sergey = new Client('Sergey Pavlovich Mishkin', true);
+Sergey.setCreditAccount('RUR', 120000, 100000, '09.09.2029', true, '02.02.2022');
+bank.push(Sergey);
+let Petrusha = new Client('Pet viktorovich Nagirin', false);
+bank.push(Petrusha);
+Petrusha.setCreditAccount('USD', 1200, 2000, '19.09.2029', false, '20.01.2019');
 
+
+async function requestExchangeRate() {
+  let promise = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+    .then(rate => rate.json())
+    .then(data => {
+      const exchangeRates = {
+        'UAH': { sale: '1'},
+      };
+      for (let rate of data) {
+        exchangeRates[rate.ccy] = rate;
+      }
+      return exchangeRates;
+    })
+    .catch(() => {
+      throw new Error('Data download error');
+    })
+  return promise;
+}
+
+async function countAllMoney() {
+  let allMoney = 0;
+  await requestExchangeRate().then(exchangeRates => {
+    for (let client of bank) {
+
+      if (client.debetAccounts.length > 0) {
+        client.debetAccounts.forEach(account => {
+          if (account.currency === 'USD') {
+            allMoney += account.balance;
+          } else {
+            let nationalValue = account.balance * exchangeRates[account.currency].sale;
+            allMoney += nationalValue / exchangeRates['USD'].sale;
+          }
+        })
+      }
+
+      if (client.creditAccounts.length > 0) {
+        client.creditAccounts.forEach(account => {
+
+          let actualBalance = account.balance - account.limit;
+          if (account.balance < account.limit) {
+            actualBalance = account.limit - account.balance;
+          } 
+
+          if (account.currency === 'USD') {
+            allMoney += actualBalance;
+          } else {
+            let nationalValue = actualBalance * exchangeRates[account.currency].sale;
+            allMoney += nationalValue / exchangeRates['USD'].sale;
+          }
+        })
+      }
+
+    }
+  })
+  return allMoney;
+}
+// countAllMoney().then(value => console.log(value))
+
+// async function findActiveCustomersDebt() {
+//   let debtAmount = 0;
+//   for (let client of bank) {
+//     if (client.isActive) {
+//       if (client.creditAccounts.length > 0) {
+//         client.creditAccounts.forEach(account => {
+//           if (account.currency === 'USD') {
+//             debtAmount += account.balance;
+//           } else {
+            
+//           }
+//         })
+//       }
+//     }
+//   }
+// }
