d85622a2d4d5b7f441f3907b9ab5290127625c0b
quarentyne <peresada1997@gmail.com>
2022-02-23 16:19:37 +0200

    first sent

--- object.js
+++ object.js
@@ -11,109 +11,41 @@ class Client{
     this.debetAccounts = [];
     this.id = Client.count++;
   }
-
-  // setDebetAccount(currency, balance, expirationDate, isActive, lastActiveDate) {
-  //   if (balance < 0) {
-  //     throw new Error('Account balance couldn\'t be negative');
-  //   }
-  //   this.debetAccounts.push({
-  //     currency: currency,
-  //     balance: balance,
-  //     expirationDate: expirationDate,
-  //     isActive: isActive,
-  //     lastActiveDate: lastActiveDate,
-  //   });
-  // }
-
-  // setCreditAccount(currency, balance, limit, expirationDate, isActive, lastActiveDate) {
-  //   if (balance < 0 || limit < 0) {
-  //     throw new Error('Balance or limit couldn\'t be negative');
-  //   }
-  //   this.creditAccounts.push({
-  //     currency: currency,
-  //     balance: balance,
-  //     limit: limit,
-  //     expirationDate: expirationDate,
-  //     isActive: isActive,
-  //     lastActiveDate: lastActiveDate,
-  //   });
-  // }
 }
 Client.count = 0;
-
-// let Petr = new Client('Petr Nikiforovich Muhin', true);
-// bank.push(Petr);
-// let Ivan = new Client('Ivan Petrovich Chushkin', true);
-// bank.push(Ivan);
-// Petr.setCreditAccount('USD', 10000, 5000, '20.01.2023', true, '21.02.2022');
-// Petr.setDebetAccount('USD', 1200, '11.12.2024', true, '21.02.2022');
-// Ivan.setDebetAccount('EUR', 100, '09.09.2029', false, '02.08.2009');
-// Ivan.setDebetAccount('RUR', 10100, '09.09.2029', false, '02.08.2009');
-// Ivan.setCreditAccount('UAH', 12000, 200000, '10.02.2022', false, '10.02.2022');
-// let Sergey = new Client('Sergey Pavlovich Mishkin', true);
-// Sergey.setCreditAccount('RUR', 1200, 100000, '09.09.2029', true, '02.02.2022');
-// bank.push(Sergey);
-// let Petrusha = new Client('Pet viktorovich Nagirin', false);
-// bank.push(Petrusha);
-// Petrusha.setCreditAccount('USD', 1200, 2000, '19.09.2029', false, '20.01.2019');
-// Petrusha.setCreditAccount('EUR', 100, 2000, '19.09.2029', false, '20.01.2019');
 
 function createNewClient(fullName, isActive) {
   let client = new Client(fullName, isActive);
   bank[client.id] = client;
 }
 
-function addCreditAccount(id, currency, balance, limit, expirationDate, isActive, lastActiveDate) {
+function addAccount(id, currency, expirationDate, isActive, lastActiveDate, balance, limit) {
   if (balance < 0 || limit < 0) {
     throw new Error('Balance or limit couldn\'t be negative');
   }
   if (id < 0) {
-    throw new Error('Invalid id was passed')
+    throw new Error('Invalid id was passed');
   }
-  bank[id].creditAccounts.push({
+
+  const accountData = {
     currency: currency,
     balance: balance,
+    expirationDate: expirationDate,
+    isActive: isActive,
+    lastActiveDate: lastActiveDate,
     limit: limit,
-    expirationDate: expirationDate,
-    isActive: isActive,
-    lastActiveDate: lastActiveDate,
-  });
-}
-
-function addDebetAccount(id, currency, balance, expirationDate, isActive, lastActiveDate) {
-  if (balance < 0) {
-    throw new Error('Account balance couldn\'t be negative');
   }
-  if (id < 0) {
-    throw new Error('Invalid id was passed')
+
+  if (limit) {
+    return bank[id].creditAccounts.push(accountData);
   }
-  bank[id].debetAccounts.push({
-    currency: currency,
-    balance: balance,
-    expirationDate: expirationDate,
-    isActive: isActive,
-    lastActiveDate: lastActiveDate,
-  });
+  return bank[id].debetAccounts.push(accountData);
 }
-
-createNewClient('Petr Nikiforovich Muhin', true);
-createNewClient('Ivan Petrovich Chushkin', true);
-createNewClient('Sergey Pavlovich Mishkin', true);
-createNewClient('Pet viktorovich Nagirin', false);
-
-addCreditAccount(0, 'USD', 10000, 5000, '20.01.2023', true, '21.02.2022');
-addDebetAccount(0, 'USD', 1200, '11.12.2024', true, '21.02.2022');
-addDebetAccount(1, 'EUR', 100, '09.09.2029', false, '02.08.2009');
-addDebetAccount(1, 'RUR', 10100, '09.09.2029', false, '02.08.2009');
-addCreditAccount(1, 'UAH', 12000, 200000, '10.02.2022', false, '10.02.2022');
-addCreditAccount(2, 'RUR', 1200, 100000, '09.09.2029', true, '02.02.2022');
-addCreditAccount(3, 'USD', 1200, 2000, '19.09.2029', false, '20.01.2019');
-addCreditAccount(3, 'EUR', 100, 2000, '19.09.2029', false, '20.01.2019');
-console.log(bank);
 
 async function requestExchangeRate() {
   let promise = await fetch('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
   let exchangeObject = await promise.json()
+    
     .then(data => {
       const exchangeRates = {};
       for (let rate of data) {
@@ -122,60 +54,59 @@ async function requestExchangeRate() {
       if (!exchangeRates['UAH']) {
         exchangeRates['UAH'] = { sale: '1' };
       }
-      // console.log(exchangeRates);
       return exchangeRates;
     })
+
     .catch(() => {
       throw new Error('Data download error');
-    })
+    });
   return exchangeObject;
 }
 
 async function countAllMoney() {
   let allMoney = 0;
   await requestExchangeRate().then(exchangeRates => {
-    for (let client of bank) {
 
+    for (let client of bank) {
       if (client.debetAccounts.length > 0) {
-        client.debetAccounts.forEach(account => {
+
+        for(let account of client.debetAccounts){
           if (account.currency === 'USD') {
             allMoney += account.balance;
-          } else {
-            let nationalValue = account.balance * exchangeRates[account.currency].sale;
-            allMoney += nationalValue / exchangeRates['USD'].buy;
+            continue;
           }
-        })
+          let nationalValue = account.balance * exchangeRates[account.currency].sale;
+          allMoney += nationalValue / exchangeRates['USD'].buy;
+        }
       }
 
       if (client.creditAccounts.length > 0) {
-        client.creditAccounts.forEach(account => {
+        for(let account of client.creditAccounts){
 
           let actualBalance = account.balance - account.limit;
           if (account.balance < account.limit) {
             actualBalance = account.limit - account.balance;
-          } 
+          }
 
           if (account.currency === 'USD') {
             allMoney += actualBalance;
-          } else {
-            let nationalValue = actualBalance * exchangeRates[account.currency].sale;
-            allMoney += nationalValue / exchangeRates['USD'].buy;
+            continue;
           }
-        })
+          let nationalValue = actualBalance * exchangeRates[account.currency].sale;
+          allMoney += nationalValue / exchangeRates['USD'].buy;
+        }
       }
-
     }
-  })
+  });
   return allMoney;
 }
-countAllMoney().then(value => console.log(value))
-
 
 function countDebt(account, exchangeRates) {
   let result = 0;
   if (account.limit < account.balance) {
     return result;
   }
+
   let debt = account.limit - account.balance;
   if (account.currency === 'USD') {
     return result += debt;
@@ -191,13 +122,12 @@ async function findCustomersDebt(isActiveAccount) {
     for (let client of bank) {
       if (client.isActive === isActiveAccount) {
         if (client.creditAccounts.length > 0) {
-          client.creditAccounts.forEach(account => {
+          for(let account of client.creditAccounts){
             debtAmount += countDebt(account, exchangeRates);
-          })
+          }
         }
       }
     }
-  })
+  });
   return debtAmount;
 }
-findCustomersDebt(false).then(value => console.log(value));
